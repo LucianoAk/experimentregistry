cd4d8fb2a4cc7986c3bc5a447fa72aacbfb80713
LucianoAk <lucianotokuno@gmail.com>
2026-08-17 02:03:02 -0300

    feat: add existsByExperimentAndNme method to repository

--- backend/src/main/java/com/lucianoak/experimentregistry/repository/ParameterRepository.java
+++ backend/src/main/java/com/lucianoak/experimentregistry/repository/ParameterRepository.java
@@ -4,6 +4,14 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.lucianoak.experimentregistry.model.Experiment;
 import com.lucianoak.experimentregistry.model.Parameter;
+import java.util.List;
 
-public interface ParameterRepository extends JpaRepository<Parameter, UUID> {}
+public interface ParameterRepository extends JpaRepository<Parameter, UUID> {
+
+    List<Parameter> findByExperiment(Experiment experiment);
+
+    boolean existsByExperimentAndName(Experiment experiment, String name);
+
+}

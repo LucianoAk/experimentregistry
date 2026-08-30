820f5a4c893b40f3fc298b41dee7a35e9162c703
LucianoAk <lucianotokuno@gmail.com>
2026-08-30 01:53:07 -0300

    chore: unify test and run script into one

--- backend/run.sh
+++ backend/run.sh
@@ -1,11 +1,82 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then
-  echo "Error: you must specify a Spring profile."
-  echo "Usage: ./run.sh <profile>"
+set -e
+
+MODE="${1:-}"
+
+if [[ -z "$MODE" ]]; then
+  echo "Error: you must specify a mode."
+  echo "Usage: ./run.sh <mode>"
   exit 1
 fi
 
-PROFILE="$1"
+start_containers() {
+  if command -v docker >/dev/null 2>&1; then
+    docker compose up -d --build
+    return
+  fi
 
-./mvnw spring-boot:run -Dspring-boot.run.profiles="$PROFILE"
+  if command -v podman >/dev/null 2>&1; then
+    podman compose up -d --build
+    return
+  fi
+
+  echo "Error: Docker or Podman is required."
+  exit 1
+}
+
+run_tests() {
+  if [[ $# -eq 0 ]]; then
+    ./mvnw test \
+      -Dspring.profiles.active="$MODE"
+    return
+  fi
+
+  local tests=()
+
+  for test in "$@"; do
+    if [[ "$test" == *.* ]]; then
+      test="${test/./#}"
+    fi
+
+    tests+=("$test")
+  done
+
+  local test_pattern
+  test_pattern=$(
+    IFS=,
+    echo "${tests[*]}"
+  )
+
+  ./mvnw test \
+    -Dspring.profiles.active="$MODE" \
+    -Dtest="$test_pattern"
+}
+
+run_spring_boot() {
+  ./mvnw spring-boot:run -Dspring-boot.run.profiles="$MODE"
+}
+
+case "$MODE" in
+--help | -h)
+  echo "Usage: ./run.sh <mode>"
+  ;;
+
+full)
+  start_containers
+  ;;
+
+dev)
+  run_spring_boot
+  ;;
+
+test)
+  run_tests "${@:2}"
+  ;;
+
+*)
+  echo "Unknown mode: $MODE"
+  echo "Run './run.sh --help' for usage."
+  exit 1
+  ;;
+esac
--- backend/src/test/java/com/lucianoak/experimentregistry/ExperimentregistryApplicationTests.java
+++ backend/src/test/java/com/lucianoak/experimentregistry/ExperimentregistryApplicationTests.java
@@ -1,13 +1,15 @@
 package com.lucianoak.experimentregistry;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class ExperimentregistryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }

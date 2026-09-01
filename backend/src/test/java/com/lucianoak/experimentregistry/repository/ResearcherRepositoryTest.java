db86572db3d58b523409886b3f41b7ad59caf13d
LucianoAk <lucianotokuno@gmail.com>
2026-09-01 18:09:16 -0300

    feat: implement tests from ResearcherRepositoryTest

--- backend/src/test/java/com/lucianoak/experimentregistry/repository/ResearcherRepositoryTest.java
+++ backend/src/test/java/com/lucianoak/experimentregistry/repository/ResearcherRepositoryTest.java
@@ -2,7 +2,9 @@ package com.lucianoak.experimentregistry.repository;
 
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,61 +37,238 @@ public class ResearcherRepositoryTest {
   @Nested
   class SearchByNameTests {
 
+    private List<Researcher> availableResearchers;
+
+    @BeforeEach
+    void setUp() {
+      availableResearchers = List.of(
+          Researcher.builder()
+              .name("John Doe")
+              .email("john.doe@example.com")
+              .build(),
+          Researcher.builder()
+              .name("John Smith")
+              .email("john.smith@example.com")
+              .build(),
+          Researcher.builder()
+              .name("John Williams")
+              .email("john.williams@example.com")
+              .build(),
+          Researcher.builder()
+              .name("Mary Johnathan")
+              .email("mary.johnathan@example.com")
+              .build(),
+          Researcher.builder()
+              .name("William Johnson")
+              .email("william.johnson@example.com")
+              .build(),
+          Researcher.builder()
+              .name("Michael Doe")
+              .email("michael.doe@example.com")
+              .build(),
+          Researcher.builder()
+              .name("MICHAEL Smith")
+              .email("michael.smith@example.com")
+              .build(),
+          Researcher.builder()
+              .name("michael Williams")
+              .email("michael.williams@example.com")
+              .build(),
+          Researcher.builder()
+              .name("MiChAeL Johnson")
+              .email("michael.johnson@example.com")
+              .build(),
+          Researcher.builder()
+              .name("Alice Bennett")
+              .email("alice.bennett@example.com")
+              .build(),
+          Researcher.builder()
+              .name("Bob Bennett")
+              .email("bob.bennett@example.com")
+              .build(),
+          Researcher.builder()
+              .name("Charlie Bennett")
+              .email("charlie.bennett@example.com")
+              .build(),
+          Researcher.builder()
+              .name("David Bennett")
+              .email("david.bennett@example.com")
+              .build());
+    }
+
+    @AfterEach
+    void tearDown() {
+      researcherRepository.deleteAll();
+      entityManager.clear();
+    }
+
+    private List<Researcher> getResearchersByNames(String... names) {
+      return List.of(names).stream()
+          .map(name -> availableResearchers.stream()
+              .filter(r -> r.getName().equals(name))
+              .findFirst()
+              .orElseThrow())
+          .toList();
+    }
+
     @Test
     void givenResearchersWithMatchingNames_whenSearchingByName_thenReturnsResearchers() {
-      Researcher researcher1 = Researcher.builder()
-          .name("John Doe")
-          .email("john@example.com")
-          .build();
+      List<Researcher> researchers = getResearchersByNames(
+          "John Doe",
+          "John Smith",
+          "John Williams");
 
-      Researcher researcher2 = Researcher.builder()
-          .name("William Johnathan")
-          .email("william@example.com")
-          .build();
-
-      entityManager.persist(researcher1);
-      entityManager.persist(researcher2);
+      researchers.forEach(entityManager::persist);
       entityManager.flush();
 
       List<Researcher> result = researcherRepository.searchByName("John");
 
       Assertions.assertEquals(
-          List.of(
-              researcher1.getName(),
-              researcher2.getName()),
+          researchers.stream()
+              .map(Researcher::getName)
+              .toList(),
+          result.stream()
+              .map(Researcher::getName)
+              .toList());
+    }
+
+    @Test
+    void givenResearchersWithPartialMatches_whenSearchingByName_thenReturnsResearchers() {
+      List<Researcher> researchers = getResearchersByNames(
+          "Mary Johnathan",
+          "William Johnson");
+
+      researchers.forEach(entityManager::persist);
+      entityManager.flush();
+
+      List<Researcher> result = researcherRepository.searchByName("ohn");
+
+      Assertions.assertEquals(
+          researchers.stream()
+              .map(Researcher::getName)
+              .toList(),
           result.stream()
               .map(Researcher::getName)
               .toList());
 
     }
 
-    // @Test
-    void givenResearchersWithPartialMatches_whenSearchingByName_thenReturnsResearchers() {
-    }
-
-    // @Test
+    @Test
     void givenResearchersWithMixedCaseNames_whenSearchingByName_thenReturnsResearchers() {
+      List<Researcher> researchers = getResearchersByNames(
+          "Michael Doe",
+          "MiChAeL Johnson",
+          "MICHAEL Smith",
+          "michael Williams");
+
+      researchers.forEach(entityManager::persist);
+      entityManager.flush();
+
+      List<Researcher> result = researcherRepository.searchByName("michael");
+
+      Assertions.assertEquals(
+          researchers.stream()
+              .map(Researcher::getName)
+              .toList(),
+          result.stream()
+              .map(Researcher::getName)
+              .toList());
+
     }
 
-    // @Test
+    @Test
     void givenResearchersWithNoMatchingNames_whenSearchingByName_thenReturnsEmptyList() {
+      List<Researcher> researchers = getResearchersByNames(
+          "John Doe",
+          "John Smith",
+          "John Williams",
+          "Mary Johnathan",
+          "William Johnson");
+
+      researchers.forEach(entityManager::persist);
+      entityManager.flush();
+
+      List<Researcher> result = researcherRepository.searchByName("Michael");
+
+      Assertions.assertEquals(
+          List.of(),
+          result.stream()
+              .map(Researcher::getName)
+              .toList());
+
     }
 
-    // @Test
+    @Test
     void givenResearchersWithPrefixAndSubstringMatches_whenSearchingByName_thenPrioritizesPrefixMatches() {
+      List<Researcher> researchers = getResearchersByNames(
+          "John Doe",
+          "John Smith",
+          "Mary Johnathan",
+          "William Johnson");
+
+      researchers.forEach(entityManager::persist);
+      entityManager.flush();
+
+      List<Researcher> result = researcherRepository.searchByName("John");
+
+      Assertions.assertEquals(
+          researchers.stream()
+              .map(Researcher::getName)
+              .toList(),
+          result.stream()
+              .map(Researcher::getName)
+              .toList());
     }
 
-    // @Test
+    @Test
     void givenMultipleMatchingResearchers_whenSearchingByName_thenReturnsResearchersAlphabetically() {
+      List<Researcher> researchers = getResearchersByNames(
+          "Alice Bennett",
+          "Bob Bennett",
+          "Charlie Bennett",
+          "David Bennett");
+
+      researchers.forEach(entityManager::persist);
+      entityManager.flush();
+
+      List<Researcher> result = researcherRepository.searchByName("Bennett");
+
+      Assertions.assertEquals(
+          researchers.stream()
+              .map(Researcher::getName)
+              .toList(),
+          result.stream()
+              .map(Researcher::getName)
+              .toList());
     }
 
-    // @Test
+  }
+
+  @Nested
+  class ExistsByEmailTests {
+
+    @AfterEach
+    void tearDown() {
+      researcherRepository.deleteAll();
+      entityManager.clear();
+    }
+
+    @Test
     void givenExistingEmail_whenCheckingIfEmailExists_thenReturnsTrue() {
+      entityManager.persistAndFlush(
+          Researcher.builder()
+              .name("John Doe")
+              .email("john@example.com")
+              .build());
+
+      boolean result = researcherRepository.existsByEmail("john@example.com");
+
+      Assertions.assertTrue(result);
     }
 
-    // @Test
+    @Test
     void givenNonExistingEmail_whenCheckingIfEmailExists_thenReturnsFalse() {
+      Assertions.assertFalse(researcherRepository.existsByEmail("john@example.com"));
     }
-
   }
 }

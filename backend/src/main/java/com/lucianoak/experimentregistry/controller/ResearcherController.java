e642a4b12bc57b18d3c09f7f51d369001482b492
LucianoAk <lucianotokuno@gmail.com>
2026-09-20 12:58:27 -0300

    chore: remove TODO comments

--- backend/src/main/java/com/lucianoak/experimentregistry/controller/ResearcherController.java
+++ backend/src/main/java/com/lucianoak/experimentregistry/controller/ResearcherController.java
@@ -39,8 +39,6 @@ public class ResearcherController {
 
   private final ResearcherService researcherService;
 
-  // TODO: Create update route
-
   @GetMapping("/search")
   public ResponseEntity<List<SearchResearcherResponseDTO>> searchByName(
       @RequestParam @Size(min = 1, max = 255, message = "Name has invalid number of characters") String name) {
